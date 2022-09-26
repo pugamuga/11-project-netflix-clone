7bf0b41027e05130ade80e4e87fb744e6d65b411
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-26 17:33:55 +0300

    add banner and react-icons

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,17 +1,36 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { baseUrl } from "../constants/movie";
 import { PugaMovie } from "../typing";
-
 
 interface IProps {
   netflixOriginal: PugaMovie[];
 }
 
 export default function Banner({ netflixOriginal }: IProps): JSX.Element {
+  const [movie, setMovie] = useState<PugaMovie | null>(null);
+  useEffect(() => {
+    setMovie(
+      netflixOriginal[Math.floor(Math.random() * netflixOriginal.length)]
+    );
+  }, [netflixOriginal]);
+
   return (
+    <div className=" flex flex-col space-y-2 md:space-y-4 py-16 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className=" absolute top-0 left-0 h-[95vh] -z-10 w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h1 className=" text-2xl lg:text-7xl md:text-4xl">{movie?.title}</h1>
+      <p className=" max-w-xs text-xs md:text-xl md:max-w-lg lg:max-w-2xl lg:text-2xl ">{movie?.overview}</p>
     <div>
-      {/* <div>
-        <Image />
-      </div> */}
+      <button className="bannerBtn">Play</button>
+      <button className="bannerBtn">More Info</button>
+    </div>
+    
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images:{
+    domains:['image.tmdb.org']
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
