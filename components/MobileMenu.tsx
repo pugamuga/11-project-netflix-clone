5a74146a96593d873a93ec658789559caca6d4d2
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-10-04 13:15:04 +0300

    create useList hook

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,7 +1,46 @@
-export default function MobileMenu():JSX.Element {
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+export default function MobileMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div>
-      
+    <div className="md:hidden !rounded-none " >
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        className="!font-netflix !text-white"
+      >
+        Browse
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        className="menu"
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem className="!font-netflix" onClick={handleClose}>Home</MenuItem>
+        <MenuItem className="!font-netflix" onClick={handleClose}>Tv shows</MenuItem>
+        <MenuItem className="!font-netflix" onClick={handleClose}>Movies</MenuItem>
+        <MenuItem className="!font-netflix" onClick={handleClose}>New & popular</MenuItem>
+        <MenuItem className="!font-netflix" onClick={handleClose}>My list</MenuItem>
+      </Menu>
     </div>
-  )
+  );
 }
---
+++ hooks/useList.tsx
@@ -0,0 +1,7 @@
+export default function useList():JSX.Element {
+  return (
+    <div>
+      
+    </div>
+  )
+}
